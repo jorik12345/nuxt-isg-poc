c92af5ea266f6e70abb71692ac9f5f192e0f5c82
Jorik Rovers <jr@ehvg.nl>
2023-06-26 13:48:07 +0200

    Nuxt3 + NetlifyCMS init

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+
+  routeRules: {
+    // Homepage pre-rendered at build time
+    "/": { prerender: true },
+    // Product page generated on-demand, revalidates in background
+    "/products/**": { swr: true },
+    // Blog post generated on-demand once until next deploy
+    "/blog/**": { isr: true },
+    // Admin dashboard renders only on client-side
+    "/admin/**": { ssr: false },
+    // Add cors headers on API routes
+    "/api/**": { cors: true },
+  },
+});
